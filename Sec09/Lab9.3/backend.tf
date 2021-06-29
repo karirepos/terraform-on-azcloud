4fbd5dee168a4690af113d87726e2657aeb4e7ef
karirepos <86487486+karirepos@users.noreply.github.com>
2021-06-29 21:55:17 +0530

    Update backend.tf

--- Sec09/Lab9.3/backend.tf
+++ Sec09/Lab9.3/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend azurerm {
       resource_group_name  = "Terra-rg"
-      storage_account_name = "remotesa01"
+      storage_account_name = "kariremotesa01"
       container_name       = "tfstate"
       key                  = "Lab93.tfstate"
   }
